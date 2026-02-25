a2a0f9d5e90d77c10dbbc64df9b8c52a1d7cbff0
Parmesh Bhatt <parmesh@weetechsolution.com>
2026-02-25 18:10:16 +0530

    refactor: convert expires time of token into seconds before use

--- src/app/jwt/helper/util.ts
+++ src/app/jwt/helper/util.ts
@@ -1,3 +1,15 @@
 export const convertIntoNumber = (value: string) => {
     return Number(value);
 }
+
+export function convertToSeconds(time: string): number {
+    const unit = time.slice(-1);
+    const value = parseInt(time.slice(0, -1));
+    switch (unit) {
+        case 's': return value;
+        case 'm': return value * 60;
+        case 'h': return value * 3600;
+        case 'd': return value * 86400;
+        default: throw new Error('Invalid time unit');
+    }
+}
--- src/app/jwt/jwt.config.ts
+++ src/app/jwt/jwt.config.ts
@@ -2,7 +2,7 @@ import { registerAs } from "@nestjs/config"
 import { getEnvVal, isProd } from "../common/helper"
 import * as fs from 'fs';
 import { JwtSignOptions } from "@nestjs/jwt";
-import { convertIntoNumber } from "./helper/util";
+import { convertIntoNumber, convertToSeconds } from "./helper/util";
 
 declare global {
     interface EnvVar {
@@ -18,8 +18,8 @@ declare global {
 
 export const jwtConfig = registerAs("jwtConfig", () => {
 
-    const accessTokenExpireIn = convertIntoNumber(getEnvVal('JWT_ACCESS_TOKEN_EXPIRES_IN', isProd() ? '8h' : '15m'))
-    const refreshTokenExpireIn = convertIntoNumber(getEnvVal('JWT_REFRESH_TOKEN_EXPIRES_IN', isProd() ? '30d' : '1d'))
+    const accessTokenExpireIn = convertToSeconds(getEnvVal('JWT_ACCESS_TOKEN_EXPIRES_IN', isProd() ? '8h' : '15m'))
+    const refreshTokenExpireIn = convertToSeconds(getEnvVal('JWT_REFRESH_TOKEN_EXPIRES_IN', isProd() ? '30d' : '1d'))
     
     let privateKey = getEnvVal("JWT_AUTH_PRIVATE_SECRET", '');
     let publicKey = getEnvVal("JWT_AUTH_PUBLIC_SECRET", '');
