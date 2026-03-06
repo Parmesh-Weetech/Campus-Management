d4ce0b78407706eb16418a7f2e9a252cf7c70397
Parmesh Bhatt <parmesh@weetechsolution.com>
2026-03-06 17:40:21 +0530

    refactor: use global server variable for creating http server

--- src/app/attendance/attendance-reader.service.ts
+++ src/app/attendance/attendance-reader.service.ts
@@ -12,7 +12,6 @@ export class AttendanceReaderService {
     ) { }
 
     async findByStudentDateClass(studentId: string, date: string, className: string): Promise<Attendance | null> {
-        console.log(date, className, studentId)
         return await this.attendanceRepository.findOne({
             where: {
                 student: { id: studentId },
